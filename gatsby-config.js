795139ba2ffca8ea461cf5a1f17b86a0089525b5
Tilman Miraß <mail@tlmn.io>
2020-11-22 16:47:10 +0100

    remove google font

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,17 +32,6 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Source Sans Pro`,
-            variants: [`400`, `700`]
-          }
-        ]
-      }
-    },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
